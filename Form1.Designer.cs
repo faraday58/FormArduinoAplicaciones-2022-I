407c7f68fb70b9d18ef8e4c8ecfcd24585835ab0
Armando Rodríguez <jarodriguezvera@gmail.com>
2021-12-06 13:30:36 -0600

    Puertos

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,18 +31,19 @@ namespace FormArduinoAplicaciones_2022_I
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.configuraciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aplicacionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pruebaLEDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.temperaturaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.semáforoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.puertosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.velocidadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
             this.verficarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.conectarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.desconectarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.pruebaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.aplicacionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.pruebaLEDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.temperaturaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.semáforoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.configuraciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tlsCmbPuertos = new System.Windows.Forms.ToolStripComboBox();
+            this.tlsCmbVelocidad = new System.Windows.Forms.ToolStripComboBox();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -54,66 +55,9 @@ namespace FormArduinoAplicaciones_2022_I
             this.aplicacionesToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1695, 42);
+            this.menuStrip1.Size = new System.Drawing.Size(2001, 42);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
-            // 
-            // configuraciónToolStripMenuItem
-            // 
-            this.configuraciónToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.puertosToolStripMenuItem,
-            this.velocidadToolStripMenuItem,
-            this.toolStripMenuItem1,
-            this.verficarToolStripMenuItem,
-            this.conectarToolStripMenuItem,
-            this.desconectarToolStripMenuItem,
-            this.pruebaToolStripMenuItem});
-            this.configuraciónToolStripMenuItem.Name = "configuraciónToolStripMenuItem";
-            this.configuraciónToolStripMenuItem.Size = new System.Drawing.Size(185, 38);
-            this.configuraciónToolStripMenuItem.Text = "&Configuración";
-            // 
-            // puertosToolStripMenuItem
-            // 
-            this.puertosToolStripMenuItem.Name = "puertosToolStripMenuItem";
-            this.puertosToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
-            | System.Windows.Forms.Keys.P)));
-            this.puertosToolStripMenuItem.Size = new System.Drawing.Size(401, 44);
-            this.puertosToolStripMenuItem.Text = "Puertos";
-            // 
-            // velocidadToolStripMenuItem
-            // 
-            this.velocidadToolStripMenuItem.Name = "velocidadToolStripMenuItem";
-            this.velocidadToolStripMenuItem.Size = new System.Drawing.Size(401, 44);
-            this.velocidadToolStripMenuItem.Text = "Velocidad";
-            // 
-            // toolStripMenuItem1
-            // 
-            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(398, 6);
-            // 
-            // verficarToolStripMenuItem
-            // 
-            this.verficarToolStripMenuItem.Name = "verficarToolStripMenuItem";
-            this.verficarToolStripMenuItem.Size = new System.Drawing.Size(401, 44);
-            this.verficarToolStripMenuItem.Text = "Verficar";
-            // 
-            // conectarToolStripMenuItem
-            // 
-            this.conectarToolStripMenuItem.Name = "conectarToolStripMenuItem";
-            this.conectarToolStripMenuItem.Size = new System.Drawing.Size(401, 44);
-            this.conectarToolStripMenuItem.Text = "Conectar";
-            // 
-            // desconectarToolStripMenuItem
-            // 
-            this.desconectarToolStripMenuItem.Name = "desconectarToolStripMenuItem";
-            this.desconectarToolStripMenuItem.Size = new System.Drawing.Size(401, 44);
-            this.desconectarToolStripMenuItem.Text = "Desconectar";
-            // 
-            // pruebaToolStripMenuItem
-            // 
-            this.pruebaToolStripMenuItem.Name = "pruebaToolStripMenuItem";
-            this.pruebaToolStripMenuItem.Size = new System.Drawing.Size(401, 44);
-            this.pruebaToolStripMenuItem.Text = "Prueba";
             // 
             // aplicacionesToolStripMenuItem
             // 
@@ -150,16 +94,87 @@ namespace FormArduinoAplicaciones_2022_I
             this.semáforoToolStripMenuItem.Size = new System.Drawing.Size(364, 44);
             this.semáforoToolStripMenuItem.Text = "Semáforo";
             // 
+            // puertosToolStripMenuItem
+            // 
+            this.puertosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tlsCmbPuertos});
+            this.puertosToolStripMenuItem.Name = "puertosToolStripMenuItem";
+            this.puertosToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.P)));
+            this.puertosToolStripMenuItem.Size = new System.Drawing.Size(401, 44);
+            this.puertosToolStripMenuItem.Text = "Puertos";
+            // 
+            // velocidadToolStripMenuItem
+            // 
+            this.velocidadToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tlsCmbVelocidad});
+            this.velocidadToolStripMenuItem.Name = "velocidadToolStripMenuItem";
+            this.velocidadToolStripMenuItem.Size = new System.Drawing.Size(401, 44);
+            this.velocidadToolStripMenuItem.Text = "Velocidad";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(398, 6);
+            // 
+            // verficarToolStripMenuItem
+            // 
+            this.verficarToolStripMenuItem.Name = "verficarToolStripMenuItem";
+            this.verficarToolStripMenuItem.Size = new System.Drawing.Size(401, 44);
+            this.verficarToolStripMenuItem.Text = "Verficar";
+            this.verficarToolStripMenuItem.Click += new System.EventHandler(this.verficarToolStripMenuItem_Click);
+            // 
+            // conectarToolStripMenuItem
+            // 
+            this.conectarToolStripMenuItem.Name = "conectarToolStripMenuItem";
+            this.conectarToolStripMenuItem.Size = new System.Drawing.Size(401, 44);
+            this.conectarToolStripMenuItem.Text = "Conectar";
+            // 
+            // desconectarToolStripMenuItem
+            // 
+            this.desconectarToolStripMenuItem.Name = "desconectarToolStripMenuItem";
+            this.desconectarToolStripMenuItem.Size = new System.Drawing.Size(401, 44);
+            this.desconectarToolStripMenuItem.Text = "Desconectar";
+            // 
+            // configuraciónToolStripMenuItem
+            // 
+            this.configuraciónToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.puertosToolStripMenuItem,
+            this.velocidadToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.verficarToolStripMenuItem,
+            this.conectarToolStripMenuItem,
+            this.desconectarToolStripMenuItem});
+            this.configuraciónToolStripMenuItem.Name = "configuraciónToolStripMenuItem";
+            this.configuraciónToolStripMenuItem.Size = new System.Drawing.Size(185, 38);
+            this.configuraciónToolStripMenuItem.Text = "&Configuración";
+            // 
+            // tlsCmbPuertos
+            // 
+            this.tlsCmbPuertos.Name = "tlsCmbPuertos";
+            this.tlsCmbPuertos.Size = new System.Drawing.Size(121, 40);
+            // 
+            // tlsCmbVelocidad
+            // 
+            this.tlsCmbVelocidad.Items.AddRange(new object[] {
+            "2400",
+            "4800",
+            "9600",
+            "19200"});
+            this.tlsCmbVelocidad.Name = "tlsCmbVelocidad";
+            this.tlsCmbVelocidad.Size = new System.Drawing.Size(121, 40);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1695, 717);
+            this.ClientSize = new System.Drawing.Size(2001, 875);
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.IsMdiContainer = true;
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Aplicaciones Arduino";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
@@ -171,6 +186,10 @@ namespace FormArduinoAplicaciones_2022_I
         #endregion
 
         private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aplicacionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pruebaLEDToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem temperaturaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem semáforoToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem configuraciónToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem puertosToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem velocidadToolStripMenuItem;
@@ -178,11 +197,8 @@ namespace FormArduinoAplicaciones_2022_I
         private System.Windows.Forms.ToolStripMenuItem verficarToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem conectarToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem desconectarToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem pruebaToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem aplicacionesToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem pruebaLEDToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem temperaturaToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem semáforoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox tlsCmbPuertos;
+        private System.Windows.Forms.ToolStripComboBox tlsCmbVelocidad;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,13 +1,25 @@
 ﻿using System;
+using System.IO.Ports;
 using System.Windows.Forms;
 
 namespace FormArduinoAplicaciones_2022_I
 {
     public partial class Form1 : Form
     {
+        private SerialPort serialPort;
         public Form1()
         {
             InitializeComponent();
+            serialPort = new SerialPort();
+            Selecciona();
+        }
+
+        public void Selecciona()
+        {
+            string[] puertos = SerialPort.GetPortNames();
+            Array.Sort(puertos);
+            tlsCmbPuertos.Items.AddRange(puertos);
+
         }
 
         private void pruebaLEDToolStripMenuItem_Click(object sender, EventArgs e)
@@ -22,6 +34,11 @@ namespace FormArduinoAplicaciones_2022_I
             FormTemperatura formTemperatura = new FormTemperatura();
             formTemperatura.MdiParent = this;
             formTemperatura.Show();
+
+        }
+
+        private void verficarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
 
         }
     }
